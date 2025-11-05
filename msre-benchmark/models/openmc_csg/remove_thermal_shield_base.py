ab34fed28b905fdb82874766da9493198ff67ce4
dmitri-scott-210 <dmitri.scott.210@gmail.com>
2025-11-05 13:38:13 -0700

    Delete msre-benchmark/models directory

--- msre-benchmark/models/openmc_csg/remove_thermal_shield_base.py
+++
@@ -1,9 +0,0 @@
-import openmc
-
-xs = '80'
-
-model = openmc.Model.from_model_xml(f'msre_csg_endf{xs}.xml')
-thermal_shield_base = model.geometry.get_all_material_cells()[2296]
-thermal_shield_base.fill = None
-
-model.export_to_model_xml(f'msre_csg_endf{xs}_no_base.xml')
